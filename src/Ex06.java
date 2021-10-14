0964c6e9aa066bce89331160229a13802182308e
Ivan de Siqueira <ivandesiqueira@gmail.com>
2021-10-13 21:17:34 -0300

    Correção

--- src/Ex06.java
+++ src/Ex06.java
@@ -7,7 +7,7 @@ public class Ex06 {
 		/*
 		 * 6 – Uma loja oferece um desconto de 3% aos clientes que tiverem o cupom
 		 * “DIADEFESTA”. Faça um programa onde o funcionário digite o valor da compra e
-		 * um cupom, informando o valor final a ser pago.!!!
+		 * um cupom, informando o valor final a ser pago.
 		 */
 
 		Scanner leitor = new Scanner(System.in);

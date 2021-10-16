cd374eed733cea29cff4bcf829e0eef95c8ecec4
Ivan de Siqueira <ivandesiqueira@gmail.com>
2021-10-16 14:06:20 -0300

    Adicionando exercícios

--- src/ExLoop01.java
+++ src/ExLoop01.java
@@ -2,7 +2,13 @@
 public class ExLoop01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * 1. Faça um programa que receba 100 valores digitados pelo usuário e, ao
+		 * final, informe qual é a soma deles.
+		 */
+		
+		
 
 	}
 
--- src/ExLoop03.java
+++ src/ExLoop03.java
@@ -1,4 +1,12 @@
 
 public class ExLoop03 {
 
+	public static void main(String[] args) {
+
+		/*
+		 * 3 . Faça um programa capaz de exibir todos os valores pares entre 2 e um
+		 * valor fornecido pelo usuário.
+		 */
+	}
+
 }
